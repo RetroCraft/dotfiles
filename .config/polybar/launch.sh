a8f76ef114770feb7074355ad3a326ae9919463e
James Ah Yong <retrocraft314@gmail.com>
2018-08-24 00:56:02 -0400

    polybar: tray

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,4 +6,5 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 [ -f ~/.cache/wal/colors.sh ] && . ~/.cache/wal/colors.sh
 export color0_alpha="#DD${color0/'#'}"
 
-polybar example &
+polybar top &
+polybar bottom &
